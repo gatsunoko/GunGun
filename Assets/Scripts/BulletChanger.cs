77428031f68ce6309ae271e41259330a229805e1
gatsuonrails <gatsuonrails@gmail.com>
2019-06-29 04:15:00 +0900

    Wepon Change Added

---
+++ Assets/Scripts/BulletChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletChanger : MonoBehaviour {
+
+  GameObject Player;
+  PlayerScript playerScript;
+  public int bulletNumber = 0;
+  Animator animator;
+
+  void Start() {
+    Player = PlayerScript.playerScript.gameObject;
+    playerScript = PlayerScript.playerScript;
+    animator = GetComponent<Animator>();
+  }
+
+  void Update() {
+  }
+
+  private void OnTriggerEnter2D(Collider2D col) {
+    if (col.gameObject.tag== "PlayerAttack") {
+      playerScript.currentWepon = bulletNumber;
+      animator.SetTrigger("Dead");
+    }
+  }
+
+  void DestroyMe() {
+    Destroy(gameObject);
+  }
+}
--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -6,11 +6,12 @@ public class PlayerScript : MonoBehaviour {
   public static PlayerScript playerScript;
 
   Rigidbody2D rigid2d;
-  public GameObject bullet;
+  public GameObject[] bullet;
   private Vector3 position;// 位置座標
   private Vector3 screenToWorldPointPosition;// スクリーン座標をワールド座標に変換した位置座標
   public bool alive = true;
   float machinegunTime = 0;
+  public int currentWepon = 0;
 
   void Awake() {
     playerScript = this;
@@ -30,14 +31,21 @@ public class PlayerScript : MonoBehaviour {
       var angle = (Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg);
       transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
 
-      if (Input.GetMouseButtonDown(0)) {
-        Attack();
+      if (currentWepon == 0) {
+        if (Input.GetMouseButtonDown(0)) {
+          Attack();
+        }
       }
-      if (Input.GetMouseButton(0)) {
-        machinegunTime += Time.deltaTime;
-        if (machinegunTime >= 0.1f) {
-          machinegunTime = 0;
-          Machinegun();
+      else if (currentWepon == 1) {
+        if (Input.GetMouseButton(0)) {
+          machinegunTime += Time.deltaTime;
+          if (machinegunTime >= 0.1f) {
+            machinegunTime = 0;
+            Machinegun();
+          }
+        }
+        else {
+          machinegunTime = 1.0f;
         }
       }
     }
@@ -45,7 +53,7 @@ public class PlayerScript : MonoBehaviour {
 
   void Attack() {
     var parent = this.transform;
-    GameObject shot = Instantiate(bullet, parent) as GameObject;
+    GameObject shot = Instantiate(bullet[0], parent) as GameObject;
     if (transform.localScale.x > 0) {
       shot.transform.localPosition = new Vector2(1.386f, 0);
       shot.transform.SetParent(null);//親子関係解消
@@ -55,7 +63,7 @@ public class PlayerScript : MonoBehaviour {
 
   void Machinegun() {
     var parent = this.transform;
-    GameObject shot = Instantiate(bullet, parent) as GameObject;
+    GameObject shot = Instantiate(bullet[1], parent) as GameObject;
     if (transform.localScale.x > 0) {
       shot.transform.localPosition = new Vector2(1.386f, 0);
       shot.transform.SetParent(null);//親子関係解消
